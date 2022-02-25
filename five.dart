ffbc658025102de25bbe94af8bf41c315da5f262
Rishika112001 <99496586+Rishika112001@users.noreply.github.com>
2022-02-25 21:34:58 +0530

    q5

---
+++ five.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+double main("unit consumed");
+double? n = double.parse(stdin.readLineSync()!);
+if(n<=200)
+{
+  double a=0.5*n;
+  print('bill is $a');
+}
+else if ((n>200)&&(n<=500))
+{
+  doube a=1*n;
+  print(bill is $a');
+  else if ((n>500)&&(n<=1000))
+{
+  doube a=2.5*n;
+  print(bill is $a');
+  else if ((n>1000)&&(n<=1500))
+{
+  doube a=3.5*n;
+  print(bill is $a');
+  else if ((n>1500)&&(n<=2500))
+{
+  doube 5=1*n;
+  print(bill is $a');
+    else 
+{
+  doube a=10*n;
+  print(bill is $a');
+  }
+  return 0;
+  }

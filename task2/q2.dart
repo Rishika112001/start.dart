6284ec761c8db10c21a654130fb3802436fa7f15
Rishika112001 <99496586+Rishika112001@users.noreply.github.com>
2022-02-26 21:03:38 +0530

    q2done

--- task2/q2.dart
+++ task2/q2.dart
@@ -1,1 +1,15 @@
-
+void main() {
+  List<int> a=[1,4,9,16,25,36,49,64,81,100];
+  
+  int i=0;
+  List<int> 1 = [];
+  
+  for(var e in a) {
+    if (++i % 2 == 0) {
+      l.add(e);
+    }
+  }
+  print(l);
+  
+  print([for (var e in a) if (++i %2 == 0) e]);
+}

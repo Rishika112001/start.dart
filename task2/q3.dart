37796d5a67b016535083634b587a412c7b91733c
Rishika112001 <99496586+Rishika112001@users.noreply.github.com>
2022-02-26 21:25:24 +0530

    q3dd

---
+++ task2/q3.dart
@@ -0,0 +1,15 @@
+void main()
+{
+           birthday={'Rishu':'13/08/2002',
+            'Dikshanshu':'26/08/2002',
+            'Rishika':'11/12/2001',
+            'Raushani':15/1/2002'}
+            
+            for  name in birthdays:
+            print(name)
+            print('birthday of which friend?')
+            name = input()
+            if   name in birthdays :
+                 print('{}\'s birthday is {}.'.format(name,birthdays[name]))
+              else
+    
